52afd65ab4117a44e25359c79440e1106804ad7e
lcouedor <leo.couedor@gmail.com>
2022-11-18 22:49:21 +0100

    movement, platform & debut collision

--- script2.js
+++ script2.js
@@ -1,178 +1,135 @@
 addEventListener('DOMContentLoaded', () => {
+    const canvas = document.querySelector('canvas')
+    const c = canvas.getContext('2d')
 
-    (function () {
-        var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-        window.requestAnimationFrame = requestAnimationFrame;
-    })();
-    
-    var canvas = document.getElementById("canvas"),
-        ctx = canvas.getContext("2d"),
-        width = 1000,
-        height = 400,
-        player = {
-            x: width / 2,
-            y: 200,
-            width: 25,
-            height: 25,
-            speed: 3,
-            velX: 0,
-            velY: 0,
-            jumping: false,
-            grounded: false,
-        },
-        keys = [],
-        friction = 0.8,
-        gravity = 0.4,
-        boxes = []
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
 
-    var bgPlayer = new Image();
-    bgPlayer.src = "img/marioRun1.png";
+    const gravity = 1.5
+    class Player{
+        constructor(){
+            this.position = {
+                x : 100,
+                y : 100
+            }
+            this.velocity = {
+                x : 0,
+                y : 10
+            }
+            this.width = 30
+            this.height = 30
+            this.jumpHeight = 20
+            this.speed = 5
+        }
+    
+        draw(){
+            c.fillStyle = "red"
+            c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        }
 
-    ctx.drawImage(bgPlayer,0,0);  
-    
-    // dimensions
-    boxes.push({//box on left
-        x: 0,
-        y: 0,
-        width: 10,
-        height: height,
-        color: 'green'
-    });
-    boxes.push({//box for the ground
-        x: 0,
-        y: height - 10,
-        width: width,
-        height: 50,
-        color: 'orange'
-    });
-    boxes.push({//box on right
-        x: width - 10,
-        y: 0,
-        width: 50,
-        height: height,
-        color: 'yellow'
-    });
-    boxes.push({ //platform
-        x: 100,
-        y: 350,
-        width: 260,
-        height: 10,
-        color: 'blue'
-    });
-    
-    canvas.width = width;
-    canvas.height = height;
-    
-    function update() {
-        // check keys
-        if (keys[38] || keys[32] || keys[87]) {
-            // up arrow or space
-            if (!player.jumping && player.grounded) {
-                player.jumping = true;
-                player.grounded = false;
-                player.velY = -player.speed * 2.5;//how high to jump
+        update(){
+            this.draw()
+            this.position.x+=this.velocity.x
+            this.position.y+=this.velocity.y
+            if (this.position.y + this.height + this.velocity.y <= canvas.height)
+                this.velocity.y += gravity
+            else this.velocity.y = 0
+        }
+    }
+
+    class Platform {
+        constructor() {
+            this.position = {
+                x: 200,
+                y: 750
             }
+            this.width = 100
+            this.height = 200
         }
-        if (keys[39] || keys[68]) {
-            // right arrow
-            if (player.velX < player.speed) {
-                player.velX++;
-            }
+
+        draw() {
+            c.fillStyle = 'blue'
+            c.fillRect(this.position.x, this.position.y, this.width, this.height)
         }
-        if (keys[37] || keys[65]) {
-            // left arrow
-            if (player.velX > -player.speed) {
-                player.velX--;
-            }
-        }
-      
-          
-    
-        player.velX *= friction;
-        player.velY += gravity;
-    
-        ctx.clearRect(0, 0, width, height);
-        ctx.beginPath();
-        
-        player.grounded = false;
-        for (var i = 0; i < boxes.length; i++) {//print boxes
-            ctx.fillStyle = boxes[i].color;
-            ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
-            
-            var dir = colCheck(player, boxes[i]);
-    
-            if (dir === "l" || dir === "r") {
-                player.velX = 0;
-                player.jumping = false;
-            } else if (dir === "b") {
-                player.grounded = true;
-                player.jumping = false;
-            } else if (dir === "t") {
-                player.velY *= -1;
-            }
-    
-        }
-        
-        if(player.grounded){
-             player.velY = 0;
-        }
-        
-        player.x += player.velX;
-        player.y += player.velY;
-      
-        ctx.fill();//Draw charater stuff
-        ctx.fillStyle = player.color;
-        ctx.fillRect(player.x, player.y, player.width, player.height);
-    
-        requestAnimationFrame(update);
     }
     
-    function colCheck(shapeA, shapeB) {
-        // get the vectors to check against
-        var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
-            vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
-            // add the half widths and half heights of the objects
-            hWidths = (shapeA.width / 2) + (shapeB.width / 2),
-            hHeights = (shapeA.height / 2) + (shapeB.height / 2),
-            colDir = null;
-    
-        // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
-        if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
-            // figures out on which side we are colliding (top, bottom, left, or right)
-            var oX = hWidths - Math.abs(vX),
-                oY = hHeights - Math.abs(vY);
-            if (oX >= oY) {
-                if (vY > 0) {
-                    colDir = "t";
-                    shapeA.y += oY;
-                } else {
-                    colDir = "b";
-                    shapeA.y -= oY;
-                }
-            } else {
-                if (vX > 0) {
-                    colDir = "l";
-                    shapeA.x += oX;
-                } else {
-                    colDir = "r";
-                    shapeA.x -= oX;
-                }
-            }
-        }
-        return colDir;
-    }
-    
-    document.body.addEventListener("keydown", function (e) {
-        keys[e.keyCode] = true;
-    });
-    
-    document.body.addEventListener("keyup", function (e) {
-        keys[e.keyCode] = false;
-    });
-    
-    
-    window.addEventListener("load", function () {
-        update();
-    });
+    const player = new Player()
+    const platform = new Platform()
 
-});
+    const keys = {
+        right: {
+            pressed: false
+        }, 
+        left: {
+            pressed: false
+        }
+    }
+
+    function animate(){
+        requestAnimationFrame(animate)
+        c.clearRect(0,0,canvas.width,canvas.height)
+        
+        platform.draw()
+        player.update()
+
+        if (keys.right.pressed) {
+            player.velocity.x = player.speed
+        } else if (keys.left.pressed) {
+            player.velocity.x = -player.speed
+        } else player.velocity.x = 0
+
+        // platform collision detection
+        //jump on platform
+        if (player.position.y + player.height <= platform.position.y &&
+            player.position.y + player.height + player.velocity.y >= platform.position.y && 
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x <= platform.position.x + platform.width){
+                player.velocity.y = 0
+        }
+
+        //gauche droite
+        if(!(player.position.x+player.width+player.speed<=platform.position.x || //TODO finir collision
+            player.position.y<=platform.position.y || 
+            player.position.x>=platform.position.x+platform.width ||
+            player.position.y>=platform.position.y+platform.height)) player.velocity.x = 0
+        
+    }
+    animate()
+
+    addEventListener('keydown', ({ keyCode }) => {
+        switch (keyCode) {
+            case 37:
+                keys.left.pressed = true
+                break
+            case 39:
+                keys.right.pressed = true
+                break
+            case 38:
+                if(player.velocity.y == 0){ //TODO essayer de bloquer le saut quand on tape dedans
+                    // console.log(player.position.y+player.jumpHeight-platform.position.y+platform.height)
+                    // if(player.position.y-player.jumpHeight>platform.position.y+platform.height){
+                    //     console.log("contact")
+                    // }
+                    player.velocity.y-= player.jumpHeight
+                }
+                break
+        }
+    })
+
+    addEventListener('keyup', ({ keyCode }) => {
+        switch (keyCode) {
+            case 37:
+                keys.left.pressed = false
+                player.velocity.x = 0
+                break
+            case 39:
+                keys.right.pressed = false
+                player.velocity.x = 0
+                break
+            //TODO vérifier si j'ai bien tout ici (saut)
+        }
+    })
+
+})
+
+//je suis à 43:20
